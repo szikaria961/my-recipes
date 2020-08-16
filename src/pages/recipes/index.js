b7e2340223464928607ab5b28fece9d7912bd587
szikaria961 <szikaria961@gmail.com>
2020-08-16 18:28:14 -0400

    Add ingredients

--- src/pages/recipes/index.js
+++ src/pages/recipes/index.js
@@ -5,7 +5,11 @@ function Recipes({ recipes }) {
 	return (
     <React.Fragment>
     {recipes.map(item => {
-      return <Recipe key={item.id} title={item.title} />
+      return <Recipe
+        key={item.id}
+        title={item.title}
+        ingredients={item.ingredients}
+      />
     })}
     </React.Fragment>
 	);
@@ -16,6 +20,16 @@ function Recipe({ title, ingredients, instructions }) {
     <div className="recipe">
       <div className="recipe-title">
         {title}
+      </div>
+      <div className="recipe-ingredients-title">Ingredients</div>
+      <div className="recipe-ingredients">
+        <ul>
+          {ingredients.map((item, index) => {
+            return (
+              <li className="recipe-ingredients-item" key={index}>{item}</li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
