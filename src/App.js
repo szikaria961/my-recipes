76c77e79224c5d71419bedef5e2a881f6123b458
szikaria961 <szikaria961@gmail.com>
2020-08-15 21:22:10 -0400

    Add recipe component with title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+const recipe = {
+  title: 'Brown Rice and Chicken'
+}
+
 function App() {
   return (
-    <div>
-      Hello World! 
+    <React.Fragment>
+      <Recipe title={'Tofu stirfry'} />
+      <Recipe title={recipe.title} />
+    </React.Fragment>
+  );
+}
+
+function Recipe({ title, ingredients, instructions }) {
+  return (
+    <div className="recipe-title">
+      {title}
     </div>
   );
 }
