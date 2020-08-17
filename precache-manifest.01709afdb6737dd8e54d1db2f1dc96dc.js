4e7f0ea10657f1a10fca71af046c5a86ec375ace
szikaria961 <szikaria961@gmail.com>
2020-08-16 20:42:12 -0400

    Updates

---
+++ precache-manifest.01709afdb6737dd8e54d1db2f1dc96dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e7252768f2bba39bf2f5856f2cf88f5",
+    "url": "/my-recipes/index.html"
+  },
+  {
+    "revision": "d192920462cdac947b61",
+    "url": "/my-recipes/static/css/main.eaaa460c.chunk.css"
+  },
+  {
+    "revision": "ab15792a9cfc1a235c34",
+    "url": "/my-recipes/static/js/2.22799d50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-recipes/static/js/2.22799d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d192920462cdac947b61",
+    "url": "/my-recipes/static/js/main.62086859.chunk.js"
+  },
+  {
+    "revision": "afc5a09cdcc51365ee90",
+    "url": "/my-recipes/static/js/runtime-main.d0982e5a.js"
+  }
+]);
