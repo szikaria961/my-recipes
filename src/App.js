6c4f1d83ab5783a21bdfb56e31fb5122b6efbaf8
szikaria961 <szikaria961@gmail.com>
2020-08-20 21:12:29 -0400

    Add header title and styles for recipe-titles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Recipes from './pages/recipes';
 import recipes from './data/recipes.json';
+import Header from './components/header'
 
 function App() {
   return (
     <React.Fragment>
+      <Header title='My Recipes' />
       <Recipes recipes={recipes} />
     </React.Fragment>
   );
---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header({ title }) {
+  return (
+    <div className="header-title"> {title} </div>
+  );
+}
+
+export default Header;
