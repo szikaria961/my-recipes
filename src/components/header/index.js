a985cf3036a4aae3d374becbe334c62bcb0ff87e
szikaria961 <szikaria961@gmail.com>
2021-01-15 12:07:05 -0500

    Add daal recipe

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
 
 function Header({ title }) {
   return (
-    <div className="header-title"> {title} </div>
+    <div className="header-title">
+      {title}
+      <span className="recipe-header-icon">
+        <FontAwesomeIcon icon={faUtensils} />
+      </span>
+    </div>
   );
 }
 
