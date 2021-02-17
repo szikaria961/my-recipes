4d961f9035d8e4e19397ec5fdb43c55425047bc9
szikaria961 <szikaria961@gmail.com>
2021-02-17 17:28:11 -0500

    Add styling

--- src/pages/recipes/index.js
+++ src/pages/recipes/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 import './index.css';
 
 function RecipeList({ recipes }) {
